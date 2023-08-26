d2538e3d9dc35f08b230c005b3ab7a1707d696e9
Adnan <adnan.hussain.136660@gmail.com>
2023-08-26 20:27:09 +0530

    Initial Commit

---
+++ app/(user)/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='text-6xl font-bold text-rose-600'>Welcome to my nu Blog</div>
+  )
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <div></div>
+            <div></div>
+        </header>
+    );
+};
+
+export default Header;
